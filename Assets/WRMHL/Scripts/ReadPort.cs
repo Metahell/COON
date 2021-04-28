14ab99080ab2f9d3a9e3be5cc42a1f5296eaedda
Metahell <71194717+Metahell@users.noreply.github.com>
2021-04-28 13:24:13 +0200

    ReadScript
    Possibilité de lire les commandes et données du controller en temps réel

---
+++ Assets/WRMHL/Scripts/ReadPort.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+/*
+This script is used to read all the data coming from the device. For instance,
+If arduino send ->
+								{"1",
+								"2",
+								"3",}
+readQueue() will return ->
+								"1", for the first call
+								"2", for the second call
+								"3", for the thirst call
+
+This is the perfect script for integration that need to avoid data loose.
+If you need speed and low latency take a look to wrmhlReadLatest.
+*/
+
+public class ReadPort : MonoBehaviour
+{
+    int CMD;
+    public float power;
+    public int sensor;
+    public bool jumpPress = false;
+    public float buttonPress1 = 0;
+    public float buttonPress2 = 0;
+    public float buttonPress3 = 0;
+    public float buttonPress4 = 0;
+    public SerialPort sp = new SerialPort("COM3", 9600);
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sp.IsOpen)
+        {
+            try
+            {
+                ReadInfo();
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+    }
+    void ReadInfo()
+    {
+        string value = sp.ReadLine(); //Read the information
+        string[] vec3 = value.Split(',');
+        sensor = int.Parse(vec3[1]);
+        power = float.Parse(vec3[0]);
+        buttonPress1 = float.Parse(vec3[2]);
+        buttonPress2 = float.Parse(vec3[3]);
+        buttonPress3 = float.Parse(vec3[4]);
+        buttonPress4 = float.Parse(vec3[5]);
+    }
+}
